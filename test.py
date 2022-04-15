804ebcc3a1ddf601d3225d75da713af844ddf94f
Aswin Sivaraman <asivara@iu.edu>
2022-04-15 13:55:57 -0400

    rewrote test script in Ray Tune

--- test.py
+++ test.py
@@ -1,86 +1,103 @@
-import collections
-import json
 import pathlib
-
-import ast
-
-from exp_models import test_denoiser
-from exp_data import Mixtures, data_te_specialist
-from time import sleep
 import re
 import sys
+from datetime import datetime
 
-if len(sys.argv) > 1:
-    folders = sys.argv[1:]
-else:
-    raise ValueError('Expected subsequent arguments to be checkpoint paths '  
-                     'or directory.')
+import torch
+from ray import tune
 
-class Row(collections.namedtuple(
-    'Row',
-    ['checkpoint_path', 'test_metrics', 'model_name', 'model_size',
-     'is_generalist', 'speaker_id', 'loss_contrastive', 'loss_purification',
-     'finetune_duration', 'num_examples']
-)):
-    def __repr__(self):
-        return (f'{self.checkpoint_path},'
-                f'{self.test_metrics},'
-                f'{self.model_name},'
-                f'{self.model_size},'
-                f'{int(self.is_generalist)},'
-                f'{self.speaker_id},'
-                f'{int(self.loss_contrastive)},'
-                f'{int(self.loss_purification)},'
-                f'{int(self.finetune_duration)},'
-                f'{int(self.num_examples)}')
+from exp_data import Mixtures
+from exp_models import load_checkpoint, feedforward
 
 
-# print csv header row
-print(','.join(Row._fields))
+def get_timestamp() -> str:
+    # format_str = "%A, %d %b %Y %H:%M:%S %p"
+    format_str = "%Y_%b_%d"
+    result = str(datetime.now().strftime(format_str))
+    return result
 
-for f in folders:
-    try:
-        path = f.strip()
-        path = path.replace('early_stopping.txt', '')
-        accumulation = not bool('grunet' in f)
-        checkpoint_path = pathlib.Path(path)
-        if checkpoint_path.is_file():
-            config_file = checkpoint_path.with_name('config.json')
-        else:
-            config_file = checkpoint_path.joinpath('config.json')
-        if not config_file.exists():
-            raise ValueError(f'Could not find {str(config_file)}.')
-        with open(config_file, 'r') as fp:
-            config: dict = json.load(fp)
-        sp_id = ''
+
+def no_op_loss(*args, **kwargs):
+    return 0
+
+
+@torch.no_grad()
+def test_function(
+        filepath: str,
+        print_to_console: bool = True,
+        write_to_file: bool = True,
+        called_by_tune: bool = True
+):
+    use_gradient_accumulation = not bool('grunet' in filepath)
+    filepath = pathlib.Path(filepath.strip().replace('early_stopping.txt', ''))
+
+    # load the experiment configuration (should be in the same directory)
+    model, config, num_examples = load_checkpoint(filepath)
+
+    # indentify the personalization target (if there is one)
+    # and prepare the speaker-specific test sets
+    speaker_id = 200
+    if 'ray' not in str(filepath):
         try:
-            sp_id = int(re.match(r'.*_(sp|ge)(\d\d\d).*', f).group(2))
-            dataset = Mixtures(
-                sp_id, 'test', split_mixture='test', snr_mixture=(-5,5))
-            results, num_examples = test_denoiser(
-                path, data_te=dataset, accumulation=accumulation,
-                use_last=False)
+            match = re.match(r'.*_(sp|ge)(\d\d\d).*', str(filepath))
+            speaker_id = int(match.group(2))
         except AttributeError:
-            results, num_examples = test_denoiser(
-                path, data_te=data_te_specialist, accumulation=accumulation,
-                use_last=False)
-        for k, v in results.items():
-            if isinstance(ast.literal_eval(k), list):
-                k = ast.literal_eval(k).pop()
-            # print(Row(
-            #     checkpoint_path=checkpoint_path,
-            #     test_metrics=v,
-            #     model_name=config['model_name'],
-            #     model_size=config['model_size'],
-            #     is_generalist=not ('_sp' in f),
-            #     speaker_id=k,
-            #     loss_contrastive=int(config['use_loss_contrastive']),
-            #     loss_purification=int(config['use_loss_purification']),
-            #     finetune_duration=config.get('dataset_duration', 0),
-            #     num_examples=num_examples
-            # ))
-            print(f'{checkpoint_path} ({num_examples}),{v}')
-    except RuntimeError as e:
-        if 'state_dict' in str(e):
-            print(f'Skipping {f} due to mismatched model.')
-    sleep(0)
+            raise NotImplementedError('need to add support for generalists')
+    dataset = Mixtures(speaker_id,
+                       split_speech='test',
+                       split_mixture='test',
+                       snr_mixture=(-5, 5))
+
+    # run the test
+    batch = dataset(100, seed=0)
+    results = feedforward(batch.inputs, batch.targets, model,
+                          weights=None, accumulation=use_gradient_accumulation,
+                          test=True, loss_reg=no_op_loss, loss_segm=no_op_loss)
+
+    if print_to_console:
+        print(f'{filepath} ({num_examples}),{results}')
+    if write_to_file:
+        with open('log.txt', 'a') as op:
+            print(f'{filepath} ({num_examples}),{results}', file=op)
+    if called_by_tune:
+        tune.report(**results)
+        return
+    else:
+        return results
+
+
+def main(use_tune: bool = False):
+
+    if len(sys.argv) > 1:
+        folders = sys.argv[1:]
+    else:
+        p = pathlib.Path('/N/u/asivara/2022-jstsp/0408_hparams_cm').rglob(
+            'ckpt_best.pt')
+        folders = [str(f) for f in p if '619b6' in str(f)]
+    # else:
+    #     raise ValueError('Expected subsequent arguments to be checkpoint paths '
+    #                      'or directory.')
+
+    def test_wrapper(config):
+        return test_function(
+            filepath=config['filepath'],
+            called_by_tune=True
+        )
+
+    if use_tune:
+        tune.run(
+            test_wrapper,
+            config={
+                'filepath': tune.grid_search(folders)
+            },
+            resources_per_trial={'cpu': 1, 'gpu': 0.25},
+            local_dir=f'test_results-({get_timestamp()})'
+        )
+        pass
+    else:
+        for f in folders:
+            test_function(f, called_by_tune=False)
+
+
+if __name__ == '__main__':
+    main(False)
